d2dac9b34a61b7668d3889a599e24a8e1709e3c9
viettq2 <viettq2@vng.com.vn>
2025-07-16 22:57:16 +0700

    Auto build update - 22:57:16 16/7/2025

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const urlsToCache = [
 ];
 
 // Thêm timestamp để force update
-const APP_VERSION = 1752680873509;
+const APP_VERSION = 1752681436818;
 
 // Install event - cache resources
 self.addEventListener('install', (event) => {
