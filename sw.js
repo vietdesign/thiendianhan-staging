23dcb66a6ece0975c0de1332406cad951fe536a4
viettq2 <viettq2@vng.com.vn>
2025-07-17 15:55:40 +0700

    Staging build update - 15:55:40 17/7/2025

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const urlsToCache = [
 ];
 
 // Thêm timestamp để force update
-const APP_VERSION = 1752741742191;
+const APP_VERSION = 1752742539038;
 
 // Install event - cache resources
 self.addEventListener('install', (event) => {
